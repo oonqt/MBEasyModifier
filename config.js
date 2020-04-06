ab133197623068ff2e9a58e355dd053dbdfc95f3
oonqt <r0blox1053@outlook.com>
2020-04-05 18:19:29 -0700

    example paths

--- config.js
+++ config.js
@@ -1,13 +1,25 @@
 module.exports = {
-    "DashboardBasePath": "",
-    "DashboardBackupCopyPath": "",
+    "DashboardBasePath": "C:\\Users\\Luke\\Desktop\\system\\dashboard-ui",
+    "DashboardBackupPath": "E:\\EmbyData\\backups",
     "FindAndOverwrite": [
-
+        { source: "E:\\EmbyData\\ModificationHold\\movies.js", dest: "movies" },
+        { source: "E:\\EmbyData\\ModificationHold\\music.js", dest: "music" },
+        { source: "E:\\EmbyData\\ModificationHold\\tv.js", dest: "tv" },
+        { source: "E:\\EmbyData\\ModificationHold\\qualityoptions.js", dest: "bower_components\\emby-webcomponents" },
+        { source: "E:\\EmbyData\\ModificationHold\\favicon.ico", dest: "\\" },
+        { source: "E:\\EmbyData\\ModificationHold\\touchicon.png", dest: "\\" },
+        { source: "E:\\EmbyData\\ModificationHold\\touchicon72.png", dest: "\\" },
+        { source: "E:\\EmbyData\\ModificationHold\\touchicon114.png", dest: "\\" },
+        { source: "E:\\EmbyData\\ModificationHold\\touchicon144.png", dest: "\\" },
+        { source: "E:\\EmbyData\\ModificationHold\\logowhite.png", dest: "modules\\logoscreensaver" },
+        { source: "E:\\EmbyData\\ModificationHold\\logodark.png", dest: "modules\\themes" },
+        { source: "E:\\EmbyData\\ModificationHold\\logowhite.png", dest: "modules\\themes" },
+        { source: "E:\\EmbyData\\ModificationHold\\black-blue", dest: "modules\\themes" },
+        { source: "E:\\EmbyData\\ModificationHold\\dark-blue", dest: "modules\\themes" },
+        { source: "E:\\EmbyData\\ModificationHold\\blueradiance", dest: "modules\\themes" },
+        { source: "E:\\EmbyData\\ModificationHold\\wmc", dest: "modules\\themes" },
     ],
     "FindAndInsert": [
-
-    ],
-    "FindAndReplace": [
 
     ]
 }
